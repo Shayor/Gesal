72372e7fb55f286bbffe4a3f7da98adc9d891a5e
Shayor <ogunladeoluwasayo@gmail.com>
2020-02-01 21:18:40 -0800

    Css changes

--- js/app.js
+++ js/app.js
@@ -1,7 +1,19 @@
+$(window).scroll(function(){
+    if ($(window).scrollTop() >= 10) {
+        $('header').addClass('fix');
+    }
+    else {
+        $('header').removeClass('fix');
+    }
+});
 $(function(){
     $(".contact").click(function(){
         $("html, body").animate({
             scrollTop: $(".form").offset().top
         },"slow");
     })
+    $('.menu').on('click',function(){
+        $(".nav-show-on-mobile").toggleClass('active')
+        // $(".mainContent").toggleClass("active-content")
+    });
 })
